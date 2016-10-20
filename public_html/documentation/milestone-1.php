1e75ad0a235892601a46058200c298d12ce49d97
Dominic Ortega <dortega37@cnm.edu>
2016-10-20 12:36:01 -0600

    made sure everything was alright.

---
+++ public_html/documentation/milestone-1.php
@@ -0,0 +1,68 @@
+<!DOCTYPE HTML>
+	<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>PWP</title>
+	</head>
+		<body>
+			<main>
+
+			<!-- begin purpose -->
+
+			<section>
+				<h2>Purpose</h2>
+					<p>I would like to identify the different topics that my core audience appreciates and try and always keep them updated with that content.</p>
+			</section>
+
+			<!-- begin audience --->
+
+			<section>
+				<h2>Audience</h2>
+				<p>This site will be designed for those who are interested in various musical genres and other entertainment content. My audience will comprise of inquiring minds searching for entertainment information.</p>
+			</section>
+
+			<!-- begin goal -->
+
+			<section>
+				<h2>Goal</h2>
+				<p>To increase the impact of the arts on the general public.</p>
+			</section>
+
+			<!-- begin persona -->
+
+			<section>
+				<h2>Persona</h2>
+				<img src="../../img/images.jpeg" alt="images"/>
+					<ul>
+						<li>Name: Ocupada Amiga </li>
+						<li>Age: 50</li>
+						<li>Profession: Busy professional who is the CEO of a fortune 500 company. She has an unsatiable hunger for information in regards to her favorite thing outside of her professional life, music. She has a reputation for traveling the globe to attend concerts.</li>
+						<li>Technology: Primarily an iPhone user. Also uses other apple products. </li>
+						<li>Attitudes and Behaviors: She is very busy.</li>
+					</ul>
+			</section>
+
+				<!-- begin needs, goals and features -->
+
+			<section>
+				<h2>Needs, Goals And Features</h2>
+					<ul>
+						<li>Have highlighted features standout</li>
+						<li>Lots of information in a short period of time</li>
+						<li>Stay up to date</li>
+						<li>Create links to all related stories</li>
+						<li>"I don't have a lot of time."</li>
+						<li>"Use more pics."</li>
+						<li>Persona may like more stories about their favorite artists.</li>
+					</ul>
+			</section>
+
+				<!-- begin use case -->
+
+			<section>
+				<h2>Use Case</h2>
+					<p>Ocupada has a free moment and accesses our site via her iPhone. She scrolls and scans the page for posts that interest her. Once she locates a post of interest she selects the post. She has the option of leaving a comment. She has the option of being linked to related posts.</p>
+			</section>
+		</main>
+	</body>
+</html>
